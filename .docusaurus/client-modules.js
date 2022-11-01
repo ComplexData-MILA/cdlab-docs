afde55c3627b53608ddf71d5d29653d6310895fb
andy <shenyang.huang@mail.mcgill.ca>
2022-10-31 21:01:40 -0400

    update to ignore unnecessary files

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('C:\\Users\\sheny\\Desktop\\cdlab-docs\\node_modules\\infima\\dist\\css\\default\\default.css'),
-  require('C:\\Users\\sheny\\Desktop\\cdlab-docs\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
-  require('C:\\Users\\sheny\\Desktop\\cdlab-docs\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress'),
-  require('C:\\Users\\sheny\\Desktop\\cdlab-docs\\src\\css\\custom.css'),
-];
